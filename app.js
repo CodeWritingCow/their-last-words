abddae6eb65c5439f6e765486992e1fee942267e
Gary Pang <gary.pang@gmail.com>
2017-11-11 19:30:46 -0500

    Add routes
    - App.js loads lastWords.json as a dependency.
    - home route returns an array of all persons from lastWords. Each person
    is an object.
    - lastname route returns persons whose last names match user's query.
    Issue #46

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ var express 	= require("express"),
 
 var sentiment 	= require("sentiment");
 
+// Loads collection of last words
+var lastWords 	= require("./lastWords");
 
 var port = process.env.PORT || 8080;
 
 app.use(logger("dev"));
+app.use(bodyParser.json());
 
+// HOME ROUTE
 app.get("/", function(req, res) {
-	res.send("THIS IS THEIR LAST WORDS");
+	res.send(lastWords);
 });
 
-
+// GET QUOTE BY PERSON'S LAST NAME
+app.get("/lastname/:lastname", function(req, res) {
+	// res.send("YOU ARE LOOKING FOR " + req.params.lastname.toUpperCase() + "'S LAST WORDS!");
+	res.send(lastWords.filter(function(person) {
+		return person.lastName.toLowerCase() === req.params.lastname.toLowerCase();
+	}));
+	// Return error
+	// Redirect if no match is found
+});
 
 app.listen(port, function() {
 	console.log("SERVER IS RUNNING ON PORT " + port);
